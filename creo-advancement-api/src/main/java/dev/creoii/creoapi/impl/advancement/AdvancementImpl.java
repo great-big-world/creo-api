9c5533353bac602b8326bb9061babd1bb55be00d
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2023-09-18 10:23:43 -0400

    Update AdvancementImpl.java

--- creo-advancement-api/src/main/java/dev/creoii/creoapi/impl/advancement/AdvancementImpl.java
+++ creo-advancement-api/src/main/java/dev/creoii/creoapi/impl/advancement/AdvancementImpl.java
@@ -15,55 +15,66 @@ import java.util.Arrays;
 public class AdvancementImpl {
     public static void applyAdvancementInjections(Identifier identifier, Advancement.Builder builder) {
         AdvancementInjectionRegistry.getAdvancementInjectors().forEach((id, injectors) -> {
-            if (identifier.equals(id)) {
-                for (Injector injector : injectors) {
-                    switch (injector.getType()) {
-                        case CRITERIA -> {
-                            CriteriaInjector criteriaInjector = (CriteriaInjector) injector;
-                            builder.criterion(criteriaInjector.name(), criteriaInjector.criterion());
-                        }
-                        case REQUIREMENTS -> {
-                            AdvancementBuilderAccessor accessor = ((AdvancementBuilderAccessor) builder);
-                            String[][] oldReqs = accessor.getRequirements();
-                            String[][] newReqs = Arrays.copyOf(oldReqs, oldReqs.length + 1);
-                            newReqs[newReqs.length - 1] = ((RequirementsInjector) injector).requirements();
-                            accessor.setRequirements(newReqs);
-                        }
-                        case REWARDS -> builder.rewards(((RewardsInjector) injector).rewards());
-                        case DISPLAY -> {
-                            AdvancementDisplayAccessor accessor = (AdvancementDisplayAccessor) ((AdvancementBuilderAccessor) builder).getDisplay();
-                            DisplayInjector displayInjector = ((DisplayInjector) injector);
-                            AdvancementDisplay newDisplay = displayInjector.getDisplay();
-                            if (newDisplay != null) {
-                                builder.display(newDisplay);
-                                break;
-                            }
-                            if (displayInjector.getTitle() != null)
-                                accessor.setTitle(displayInjector.getTitle());
-                            if (displayInjector.getDescription() != null)
-                                accessor.setDescription(displayInjector.getDescription());
-                            if (displayInjector.getIcon() != null)
-                                accessor.setIcon(displayInjector.getIcon());
-                            if (displayInjector.getBackground() != null)
-                                accessor.setBackground(displayInjector.getBackground());
-                            if (displayInjector.getFrame() != null)
-                                accessor.setFrame(displayInjector.getFrame());
-                            if (displayInjector.isShowToast())
-                                accessor.setShowToast(displayInjector.isShowToast());
-                            if (displayInjector.isAnnounceToChat())
-                                accessor.setAnnounceToChat(displayInjector.isAnnounceToChat());
-                            if (displayInjector.isHidden())
-                                accessor.setHidden(displayInjector.isHidden());
-                        }
-                        case PARENT -> {
-                            ParentInjector parentInjector = (ParentInjector) injector;
-                            if (parentInjector.getParentId() == null)
-                                builder.parent(parentInjector.getParent());
-                            else builder.parent(parentInjector.getParentId());
-                        }
-                    }
+            if (!identifier.equals(id))
+                return;
+
+            for (Injector injector : injectors) {
+                switch (injector.getType()) {
+                    case CRITERIA -> applyCriteriaInjections((CriteriaInjector) injector, builder);
+                    case REQUIREMENTS -> applyRequirementsInjections((RequirementsInjector) injector, builder);
+                    case REWARDS -> applyRewardsInjections((RewardsInjector) injector, builder);
+                    case DISPLAY -> applyDisplayInjections((DisplayInjector) injector, builder);
+                    case PARENT -> applyParentInjections((ParentInjector) injector, builder);
                 }
             }
         });
+    }
+
+    private static void applyCriteriaInjections(CriteriaInjector injector, Advancement.Builder builder) {
+        builder.criterion(injector.name(), injector.criterion());
+    }
+
+    private static void applyRequirementsInjections(RequirementsInjector injector, Advancement.Builder builder) {
+        AdvancementBuilderAccessor accessor = (AdvancementBuilderAccessor) builder;
+        String[][] oldReqs = accessor.getRequirements();
+        String[][] newReqs = Arrays.copyOf(oldReqs, oldReqs.length + 1);
+        newReqs[newReqs.length - 1] = injector.requirements();
+        accessor.setRequirements(newReqs);
+    }
+
+    private static void applyRewardsInjections(RewardsInjector injector, Advancement.Builder builder) {
+        builder.rewards(injector.rewards());
+    }
+
+    private static void applyDisplayInjections(DisplayInjector injector, Advancement.Builder builder) {
+        AdvancementDisplayAccessor accessor = (AdvancementDisplayAccessor) ((AdvancementBuilderAccessor) builder).getDisplay();
+        AdvancementDisplay newDisplay = injector.getDisplay();
+        if (newDisplay != null) {
+            builder.display(newDisplay);
+            return;
+        }
+        if (injector.getTitle() != null)
+            accessor.setTitle(injector.getTitle());
+        if (injector.getDescription() != null)
+            accessor.setDescription(injector.getDescription());
+        if (injector.getIcon() != null)
+            accessor.setIcon(injector.getIcon());
+        if (injector.getBackground() != null)
+            accessor.setBackground(injector.getBackground());
+        if (injector.getFrame() != null)
+            accessor.setFrame(injector.getFrame());
+        if (injector.isShowToast())
+            accessor.setShowToast(injector.isShowToast());
+        if (injector.isAnnounceToChat())
+            accessor.setAnnounceToChat(injector.isAnnounceToChat());
+        if (injector.isHidden())
+            accessor.setHidden(injector.isHidden());
+    }
+
+    private static void applyParentInjections(ParentInjector injector, Advancement.Builder builder) {
+        if (injector.getParentId() == null)
+            builder.parent(injector.getParent());
+        else
+            builder.parent(injector.getParentId());
     }
 }
