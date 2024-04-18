3cb0f1eaee0967c9dcc3664ada0b74187eb932d6
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-04-17 22:06:17 -0400

    Fix a Crash when Attacking Item Entities

--- creo-item-api/src/main/java/dev/creoii/creoapi/mixin/item/ClientPlayerInteractionManagerMixin.java
+++ creo-item-api/src/main/java/dev/creoii/creoapi/mixin/item/ClientPlayerInteractionManagerMixin.java
@@ -3,6 +3,9 @@ package dev.creoii.creoapi.mixin.item;
 import dev.creoii.creoapi.impl.item.CreoItemImpl;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Final;
@@ -10,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerInteractionManager.class)
@@ -19,5 +23,11 @@ public class ClientPlayerInteractionManagerMixin {
     @Inject(method = "attackBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameMode;isCreative()Z"))
     private void creo$tryAttackThroughBlock(BlockPos pos, Direction direction, CallbackInfoReturnable<Boolean> cir) {
         CreoItemImpl.applyAttackThroughBlockClient(client);
+    }
+
+    @Inject(method = "attackEntity", at = @At("HEAD"), cancellable = true)
+    private void creo$dontAttackItemEntities(PlayerEntity player, Entity target, CallbackInfo ci) {
+        if (target instanceof ItemEntity)
+            ci.cancel();
     }
 }
