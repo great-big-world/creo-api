5f505b9ae1e409778d532e0014255b50e77bbcbb
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-05-08 21:55:00 -0400

    Improve Pre-Init Mod Initializer Javadoc

--- creo-registry-api/src/main/java/dev/creoii/creoapi/api/registry/init/PreInitModInitializer.java
+++ creo-registry-api/src/main/java/dev/creoii/creoapi/api/registry/init/PreInitModInitializer.java
@@ -3,7 +3,13 @@ package dev.creoii.creoapi.api.registry.init;
 /**
  * A mod initializer run on both {@link net.fabricmc.api.EnvType#CLIENT} and {@link net.fabricmc.api.EnvType#SERVER}.
  *
- * <p>This entrypoint is suitable for setting up logic, systems, or modifications that need to happen <i>before</i> anything in the game is registered.</p>
+ * <p>This entrypoint is suitable for setting up logic, systems, or modifications that need to happen <i>before</i> anything in the game is registered. Examples in the Vanilla game include:</p>
+ * <ul>
+ *     <li>Flammable Blocks</li>
+ *     <li>Compostable Items</li>
+ *     <li>Dispenser Behaviors</li>
+ *     <li>Cauldron Behaviors</li>
+ * </ul>
  *
  * <p>In {@code fabric.mod.json}, this entrypoint is defined with {@code pre-init} key.</p>
  *
