a7e5bb1a97b0f2a009ca0f706fd2add454d16173
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-08-28 10:42:51 -0400

    Fix server load crashes due to Block API
    Backport of change from 1.21 branch

---
+++ creo-block-api/src/main/java/dev/creoii/creoapi/api/block/CreoBlockApi.java
@@ -0,0 +1,12 @@
+package dev.creoii.creoapi.api.block;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CreoBlockApi implements ModInitializer {
+    public static final String NAMESPACE = "creo_api";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
