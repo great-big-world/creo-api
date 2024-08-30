c1a70f54ba3a2cc28b99fe3c1d57e04185f7e4b2
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-08-29 23:03:25 -0400

    Register LookAtBlock packet on both envs

--- creo-block-api/src/main/java/dev/creoii/creoapi/api/block/client/CreoBlockApiClient.java
+++ creo-block-api/src/main/java/dev/creoii/creoapi/api/block/client/CreoBlockApiClient.java
@@ -4,18 +4,14 @@ import dev.creoii.creoapi.api.block.CreoBlock;
 import dev.creoii.creoapi.impl.block.BlockImpl;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.math.BlockPos;
 
 public class CreoBlockApiClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        PayloadTypeRegistry.playS2C().register(BlockImpl.LookAtBlock.PACKET_ID, BlockImpl.LookAtBlock.PACKET_CODEC);
-
         ClientPlayNetworking.registerGlobalReceiver(BlockImpl.LookAtBlock.PACKET_ID, (payload, context) -> {
             ClientWorld world = context.client().world;
             if (world != null) {
